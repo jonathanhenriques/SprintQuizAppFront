5f316d01d5c9463e54ae38cb2b3207ebabee01b9
Jonathan Henrique <silva.henriquejonas@gmail.com>
2022-08-17 11:44:06 -0300

    Implementando área de atualização de info do usuário

---
+++ src/app/atualizar-usuario/atualizar-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atualizar-usuario',
+  templateUrl: './atualizar-usuario.component.html',
+  styleUrls: ['./atualizar-usuario.component.scss']
+})
+export class AtualizarUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
