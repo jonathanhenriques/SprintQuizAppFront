d956a31e4ff675acfea2418fd643a6d58ac19a41
Jonathan Henrique <silva.henriquejonas@gmail.com>
2022-10-13 15:08:43 -0300

    contrutor na classe Prova

--- src/app/model/Prova.ts
+++ src/app/model/Prova.ts
@@ -5,14 +5,24 @@ import { Usuario } from './Usuario';
 
 
 export class Prova {
-  public id!: number
-  public nome!: string
-  public descricao!: string
-  public duracao!: number
-  public usuario!: Usuario
-  public questoes!: QuestaoProva[]
-  public instituicao!: string
-  public categoria!: CategoriaProva
+  public id;
+  // public id!: number
+  // public nome!: string
+  // public descricao!: string
+  // public duracao!: number
+  // public usuario!: Usuario
+  // public questoes!: QuestaoProva[]
+  // public instituicao!: string
+  // public categoria!: CategoriaProva
+
+  //construtor
+  constructor(
+    id: number, nome: String, descricao: String, duracao: number, usuario: Usuario,
+     questoes: QuestaoProva[], instituicao: String, categoria: String) {
+    this.id = id;
+  }
+
+
 }
 
-//construtor
+
