d9d2040e23cafb2c98e79d275c505493674bbad5
Jonathan Henrique <silva.henriquejonas@gmail.com>
2022-12-03 18:20:02 -0300

    iniciando modificacoes para nova API com jwt

--- src/app/model/UsuarioLogin.ts
+++ src/app/model/UsuarioLogin.ts
@@ -4,7 +4,7 @@ export class UsuarioLogin {
   public id!: number;
   public nome!: string;
   public senha!: string;
-  public usuario!: string;
+  public email!: string;
   public token!: string;
   public foto!: string;
   public tipo!: string;
